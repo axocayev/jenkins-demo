04b0341098ab95522cdd15e81ddaab35b2db4a6a
Anar Xocayev <anarxocayev@gmail.com>
2021-02-27 18:12:33 +0400

    1st commit

---
+++ src/main/java/az/atlacademy/demojenkins/controller/MainController.java
@@ -0,0 +1,13 @@
+package az.atlacademy.demojenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping
+    public String home(){
+        return "Jenkins tutorial";
+
+    }
+}
