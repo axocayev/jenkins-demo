cf83ca206a27efa5c5fcc50834edca234563530f
Anar Xocayev <anarxocayev@gmail.com>
2021-02-28 15:24:40 +0400

    some text added

--- src/main/java/az/atlacademy/demojenkins/controller/MainController.java
+++ src/main/java/az/atlacademy/demojenkins/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping
     public String home(){
-        return "Jenkins tutorial";
+        return "Jenkins tutorial  some new text addded";
 
     }
 }
